2e8a29eb4a13d4be0ec5b43453e445a53ed738a3
hsh97Erica <imscs21@hanyang.ac.kr>
2017-09-24 21:07:01 +0900

    add github oauth

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # part_login
+
+mamp 사용시 포트를 8888로 바꾸고 webproj폴터를 생성해 그곳이 여기 파일들을 집어넣고 mamp를 킨다
+ http://127.0.0.1:8888/webproj/oauthtest.html 로 접속해 링크를 클릭한다
---
+++ oauthcallback.php
@@ -0,0 +1,34 @@
+<?php
+$url = 'https://github.com/login/oauth/access_token';
+  $data = array(
+    'code'=>$_GET['code'],
+  'client_secret'=>'a88c11d03ec5660db6b0df93c6f4ab1fd461ce90',
+  'client_id'=>'1287d5c5d583fd5fd520'
+);
+$content = json_encode($data);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    // post_data
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+    if (!is_null($header)) {
+        curl_setopt($curl, CURLOPT_HEADER, true);
+    }
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+    curl_setopt($curl, CURLOPT_VERBOSE, true);
+$json_response = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+if ( $status != 200 ) {
+    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+}
+curl_close($curl);
+//echo $json_response;
+$response = json_decode($json_response, true);
+$token = $response['access_token'];
+header('Location: https://api.github.com/user?access_token=' . $token);
+ ?>
