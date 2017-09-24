4e37440dcf1aea456d7dbfc959f4cf8c67edad2e
hsh97Erica <imscs21@hanyang.ac.kr>
2017-09-24 21:45:06 +0900

    add some curl codes

--- oauthcallback.php
+++ oauthcallback.php
@@ -11,6 +11,8 @@ $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+ curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
     // post_data
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
